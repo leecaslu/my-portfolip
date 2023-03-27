aad0c6bed8c130c8a81d05497b99fabe72f4f59e
leecaslu <lucashollerlee@gmail.com>
2023-03-27 19:37:22 -0300

    small changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,20 @@ import React from 'react';
 
 function App() {
   return (
-    <div className='container'>
-      <HeadersContainer/>
+    <div>
+      <div className='container' id='header'>
+        <HeadersContainer />
+      </div>
+      <div className='container' id='body'>
+
+      </div>
+      <div className='container' id='fotoLucas'>
+        <img src='http://placehold.it/' alt='placeholder'></img>
+      </div>
     </div>
-    
+
+
+
   );
 }
 
